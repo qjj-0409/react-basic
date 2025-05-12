265e3963689f06a1a41fbedab0b1401473bb7fb8
qjj <qi18790396124@163.com>
2025-05-12 10:51:06 +0800

    JSX中实现复杂条件渲染

--- React18.md
+++ React18.md
@@ -189,3 +189,36 @@ function App() {
 export default App;
 ```
 
+JSX中实现复杂条件渲染
+
+需求：列表中需要根据文章状态适配三种情况，单图、三图和无图三种模式
+
+解决方案：自定义函数 + if判断语句
+
+```jsx
+// 定义文章类型
+const articleType = 3; // 0 1 3
+
+// 定义核心函数（根据文章类型返回不同的JSX模板）
+function getArticleTem() {
+  if (articleType === 0) {
+    return <div>我是无图文章</div>
+  } else if (articleType === 1) {
+    return <div>我是单图文章</div>
+  } else {
+    return <div>我是三图文章</div>
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      {/* 调用函数渲染不同的模板 */}
+      { getArticleTem() }
+    </div>
+  );
+}
+
+export default App;
+```
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-const isLogin = true;
+// 定义文章类型
+const articleType = 3; // 0 1 3
+
+// 定义核心函数（根据文章类型返回不同的JSX模板）
+function getArticleTem() {
+  if (articleType === 0) {
+    return <div>我是无图文章</div>
+  } else if (articleType === 1) {
+    return <div>我是单图文章</div>
+  } else {
+    return <div>我是三图文章</div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      {/* 逻辑与 && */}
-      { isLogin && <span>this is span</span> }
-      {/* 三元运算 */}
-      { isLogin ? <span>jack</span> : <span>loading...</span>}
+      {/* 调用函数渲染不同的模板 */}
+      { getArticleTem() }
     </div>
   );
 }
