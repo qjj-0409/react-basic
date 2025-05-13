994da0ab10c0f2b947e5e5ef361be52084a9ef70
qjj <qi18790396124@163.com>
2025-05-13 09:48:13 +0800

    表单受控绑定

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+// 受控表单绑定
 
+import { useState } from "react"
+
+// 1.声明一个react状态 - useState
+// 2.核心绑定流程
+//   1.通过value属性绑定react状态
+//   2.绑定onChange事件 通过事件参数e拿到输入框最新的值 反向修改到react状态
 const App = () => {
-  
+  const [value, setValue] = useState('')
   return (
     <div className="app">
-      
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
   )
 }
