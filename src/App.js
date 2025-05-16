ee02f64097fb03d74e539e34979bc8a45303ec77
qjj <qi18790396124@163.com>
2025-05-16 16:33:40 +0800

    组件通信-父传子-基础实现

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+// 父传子
+// 1.父组件传递数据 子组件标签上绑定属性
+// 2.子组件接收数组 props的参数
 
+function Son(props) {
+  // props：对象里面包含了父组件传递过来的所有的数据
+  console.log(props)
+  return <div>this is son, {props.name}</div>
+}
 
 const App = () => {
+  const name = 'this is app name';
   return (
-    <div className="app"></div>
+    <div className="app">
+      <Son name={name} />
+    </div>
   )
 }
 
