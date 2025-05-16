f6b271c81e7adf207ff491706f7791d6fbba849b
qjj <qi18790396124@163.com>
2025-05-16 16:43:45 +0800

    组件通信-父传子-props说明

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@
 function Son(props) {
   // props：对象里面包含了父组件传递过来的所有的数据
   console.log(props)
-  return <div>this is son, {props.name}</div>
+  return <div>this is son, {props.name}, jsx: {props.child}</div>
 }
 
 const App = () => {
   const name = 'this is app name';
   return (
     <div className="app">
-      <Son name={name} />
+      <Son 
+        name={name}
+        age={18}
+        isTrue={false}
+        cb={() => { console.log('123') }}
+        list={['Vue', 'React']}
+        obj={{name: 'jack'}}
+        child={<span>this is span</span>}
+      />
     </div>
   )
 }
