9531b3fc593c9c9e72207f996c1388d19d6ad7eb
qjj <qi18790396124@163.com>
2025-05-19 12:39:54 +0800

    useEffect-概念理解和基础使用

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-// App -> A -> B
-// 1.createContext方法创建一个上下文对象
-// 2.在顶层组件 通过Provider组件提供数组
-// 3.在底层组件 通过useContext钩子函数使用数据
-
-import { createContext, useContext } from "react"
-
-const MsgContext = createContext()
-
-const A = () => {
-  return (
-    <div>
-      this is A component.
-      <B />
-    </div>
-  )
-}
-
-const B = () => {
-  const msg = useContext(MsgContext)
-  return (
-    <div>
-      this is B component.
-      {msg}
-    </div>
-  )
-}
+import { useEffect, useState } from "react"
+const URL = "https://geek.itheima.net/v1_0/channels"
 
 const App = () => {
-  const msg = "this is app msg"
+  const [list, setList] = useState([])
+  useEffect(() => {
+    async function getList() {
+      const res = await fetch(URL)
+      const jsonRes = await res.json()
+      console.log(jsonRes)
+      setList(jsonRes.data.channels)
+    }
+    getList()
+  }, [])
   return (
     <div>
-      <MsgContext.Provider value={msg}>
-        this is App.
-        <A />
-      </MsgContext.Provider>
+      this is app
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
