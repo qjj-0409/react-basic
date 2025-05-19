83887ef94d4a83b57aee860f4535b60daae0aff7
qjj <qi18790396124@163.com>
2025-05-19 12:51:51 +0800

    useEffect-不同依赖项说明

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react"
-const URL = "https://geek.itheima.net/v1_0/channels"
 
 const App = () => {
-  const [list, setList] = useState([])
+  const [count, setCount] = useState(0)
+  // 1.没有依赖项 初始 + 组件更新
+  // useEffect(() => {
+  //   console.log("副作用函数执行了")
+  // })
+
+  // 2.传入空数组依赖 初始执行一次
+  // useEffect(() => {
+  //   console.log("副作用函数执行了")
+  // }, [])
+
+  // 3.传入特定依赖项 初始 + 依赖项变化时执行
   useEffect(() => {
-    async function getList() {
-      const res = await fetch(URL)
-      const jsonRes = await res.json()
-      console.log(jsonRes)
-      setList(jsonRes.data.channels)
-    }
-    getList()
-  }, [])
+    console.log("副作用函数执行了")
+  }, [count])
   return (
     <div>
       this is app
-      <ul>
-        {list.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      <button onClick={() => setCount(count + 1)}>+{count}</button>
     </div>
   )
 }
