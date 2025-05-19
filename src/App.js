ab4750d3b03658e40215e02789a1204681d366a0
qjj <qi18790396124@163.com>
2025-05-19 15:18:46 +0800

    自定义Hook实现

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import { useEffect, useState } from "react"
-function Son() {
-  // 1.渲染时开启一个定时器
-  useEffect(() => {
-    const timer = setInterval(() => {
-      console.log("定时器执行中...")
-    }, 1000)
-    return () => {
-      // 清除副作用（组件卸载时）
-      console.log("清除副作用")
-      clearInterval(timer)
-    }
-  }, [])
-  return <div>this is son</div>
+// 封装自定义hook
+
+import { useState } from "react"
+
+function useToggle() {
+  // 可复用的逻辑代码
+  const [value, setValue] = useState(true)
+
+  const toggle = () => setValue(!value)
+
+  // 哪些状态和回调函数需要在其他组件中使用 return
+  return {
+    value,
+    toggle,
+  }
 }
 
+// 封装自定义hook通用思路
+// 1.声明一个以use开头的函数
+// 2.在函数体内封装可复用的逻辑（只要是可复用的逻辑）
+// 3.把组件中用到的状态或者回调return出去（以对象或者数组）
+// 4.在哪个组件中要用到这个逻辑，就执行这个函数，解构出来状态和回调进行使用
+
 const App = () => {
-  // 通过条件渲染模拟组件卸载
-  const [show, setShow] = useState(true)
+  const { value, toggle } = useToggle()
   return (
     <div>
-      {show && <Son />}
-      <button onClick={() => setShow(false)}>卸载Son组件</button>
+      {value && <div>this is div</div>}
+      <button onClick={toggle}>toggle</button>
     </div>
   )
 }
